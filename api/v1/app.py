1ba88c7317065085f4ab744fe1e2c50736285e7b
David Perez <daperezcruz@gmail.com>
2022-12-16 13:48:17 -0500

    task 4

---
+++ api/v1/app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This is a comment that is going to change
+"""
+from api.v1.views import app_views
+from models import storage
+from flask import Flask
+from os import environ
+
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close(exception):
+    """close the storage"""
+    storage.close()
+
+if __name__ == "__main__":
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+
+    app.run(host=host, port=port, threaded=True, debug=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is a comment
+"""
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status')
+def status():
+    """
+    Status of the API
+    """
+    return jsonify({'status': 'OK'})
