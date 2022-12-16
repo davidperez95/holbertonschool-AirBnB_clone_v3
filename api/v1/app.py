e7193260728e53019134b23bba434aa68f4d9132
David Perez <daperezcruz@gmail.com>
2022-12-16 15:41:51 -0500

    task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ This is a comment that is going to change
 """
 from api.v1.views import app_views
 from models import storage
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from os import environ
 
 
@@ -16,6 +16,14 @@ app.register_blueprint(app_views)
 def close(exception):
     """close the storage"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found_error(error):
+    """
+    Error handler for 404 error not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
 
 if __name__ == "__main__":
     host = environ.get('HBNB_API_HOST')
