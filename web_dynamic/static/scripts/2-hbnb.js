1eef239a2e5b90db0460ee82dc0e59cdef1e5252
David Perez <daperezcruz@gmail.com>
2023-01-06 18:44:54 -0500

    task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,42 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const amenitiesid = [];
-    const amenitiesnames = [];
-  
-    const boxes = document.querySelectorAll('.amenities input[type="checkbox"]');
-    const amenitiestitle = document.querySelector('.amenities h4');
-    const api = document.getElementById('api_status');
-  
-    boxes.forEach((amenity) => {
-      amenity.addEventListener('change', () => {
-        if (amenity.checked) {
-          amenitiesid.push(amenity.dataset.id);
-          amenitiesnames.push(amenity.dataset.name);
-  
-        } else {
-          const indexid = amenitiesid.indexOf(amenity.dataset.id);
-          if (indexid !== -1) amenitiesid.splice(indexid, 1);
-  
-          const indexname = amenitiesnames.indexOf(amenity.dataset.name);
-          if (indexname !== -1) amenitiesnames.splice(indexname, 1);
-        }
-  
-        let amenities = amenitiesnames.toString().replaceAll(',', ', ');
-  
-        if (amenities.lenght > 32) {
-          amenities = amenities.substring(0, 32) + '...'
-        }
-      
-        amenitiestitle.innerHTML = amenities;
-      });
-    });
+$(document).ready(function () {
+  const dict = {};
+  const $amenitiesCheck = $('input[type=checkbox]');
+  const $selectedAmenities = $('div.amenities h4');
+  const $statusIndicator = $('div#api_status');
+  const statusUri = 'http://localhost:5001/api/v1/status';
 
-  fetch('http://0.0.0.0:5001/api/v1/status/')
-    .then(response => response.json())
-    .then(date => {
-        if (date.status === 'OK') {
-            api.classList.add('available');
-        } else {
-            api.classList.remove('available');
-        }
-    });
+  $amenitiesCheck.click(function () {
+    if ($(this).is(':checked')) {
+      dict[$(this).data('id')] = $(this).data('name');
+      $selectedAmenities.text(Object.values(dict).join(', '));
+    } else if ($(this).is(':not(:checked)')) {
+      delete dict[$(this).data('id')];
+      $selectedAmenities.text(Object.values(dict).join(', '));
+    }
+  });
+
+  $.ajax({
+    url: statusUri,
+    type: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      if (data.status === 'OK') {
+        $statusIndicator.addClass('available');
+      } else {
+        $statusIndicator.removeClass('available');
+      }
+    }
+  });
 });
